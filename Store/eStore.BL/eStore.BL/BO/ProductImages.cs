5629da809db05c982893e0ea80d6c388a024feae
iecat <irinapauna@gmail.com>
2014-11-05 22:15:38 +0200

    add
    add

---
+++ Store/eStore.BL/eStore.BL/BO/ProductImages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore.BL.BO
+{
+    public class ProductImages
+    {
+        public int ID { get; set; }
+        public Product Product { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Store/eStore/eStore/Controllers/CheckoutController.cs
@@ -0,0 +1,37 @@
+﻿using eStore.BL.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        //
+        // GET: /Checkout/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Add(Product product)
+        {
+            List<Product> products;// = new List<Product>();
+            if(Session["AddedProducts"] !=null)
+            {
+                products = new List<Product>();
+                products.AddRange((List<Product>)Session["AddedProducts"]);
+            }
+            else
+            {
+                products = new List<Product>();
+                products.Add(product);
+                Session["AddedProducts"] = products;
+            }
+
+        }
+	}
+}
